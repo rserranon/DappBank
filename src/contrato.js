d44f3ccaa2f88e6c070bb30f9e74b52e69432270
Roberto Serrano <rserranon@gmail.com>
2016-03-26 16:32:47 -0600

    same

--- src/contrato.js
+++ src/contrato.js
@@ -1,25 +1,28 @@
 //import {computedFrom} from 'aurelia-framework';
 import Web3 from 'ethereum/web3.js';
+import {inject} from 'aurelia-framework';  
+import {ApplicationState} from './applicationState';
 
-let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+// let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
+@inject(ApplicationState)
 export class Contrato {
 
-  constructor(){
+  constructor(appState){
     // Class variables
-
-    this.selectedAccount = web3.eth.coinbase;
-    this.web3Listening = web3.net.listening;
-    this.web3IsConnected = web3.isConnected()
-    this.peerCount = web3.net.peerCount;
-    this.coinbaseAddr = web3.eth.coinbase;
-    this.coinbaseBalance = web3.fromWei(web3.eth.getBalance(web3.eth.coinbase), 'ether');
-    this.accounts = web3.eth.accounts;
-    this.selectedAccountBalance = web3.fromWei(web3.eth.getBalance(this.selectedAccount), 'ether');
+    this.web3 = appState.web3;
+    this.selectedAccount        = this.web3.eth.coinbase;
+    this.web3Listening          = this.web3.net.listening;
+    this.web3IsConnected        = this.web3.isConnected()
+    this.peerCount              = this.web3.net.peerCount;
+    this.coinbaseAddr           = this.web3.eth.coinbase;
+    this.coinbaseBalance        = this.web3.fromWei(this.web3.eth.getBalance(this.web3.eth.coinbase), 'ether');
+    this.accounts               = this.web3.eth.accounts;
+    this.selectedAccountBalance = this.web3.fromWei(this.web3.eth.getBalance(this.selectedAccount), 'ether');
   }
   
   accountChanged(cuenta) {  
-    this.selectedAccountBalance = web3.fromWei(web3.eth.getBalance(cuenta), 'ether');
+    this.selectedAccountBalance = this.web3.fromWei(this.web3.eth.getBalance(cuenta), 'ether');
   }
 }
 
